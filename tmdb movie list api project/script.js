67755fa3961f1393c85ef37a3f3d2c4851b5c123
kaliyappan <141757796+Kaliyappan1@users.noreply.github.com>
2024-01-25 21:33:09 +0530

    Add files via upload

--- tmdb movie list api project/script.js
+++ tmdb movie list api project/script.js
@@ -12,47 +12,38 @@ function myfunction() {
   console.log(new_url);
   // document.getElementById("demo").innerHTML=(new_url);
 
-  const options = {
+  fetch(new_url, {
     method: "GET",
     headers: {
       accept: "application/json",
       Authorization:
         "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MGE3ZDNmOGNiZmNiYTMwMjFjOGQwYTBkNTA1MDM2ZSIsInN1YiI6IjY1OWI1MTdiYTEwNzRiMDBlZWIwZmJhMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YLIYRQxwOX_1f3Pb4o332V5FQeZSMiae5EDo5Cuub_0",
-    },
-  };
-
-  fetch(new_url, options)
-    .then((response) => response.json())
-    // .then(response => console.log(response["results"]))
-    .then((response) => {
-      // let data = response
-      // $("#submit").click(function(){
-      //   let success= $(this).attr('submit')
-      //     $(this).attr()
-      // })
-      const data = Object.keys(response["results"]);
-      $.each(data, function () {
-        const outerKey = this;
-        const outerObject = response["results"][outerKey];
-    
-        // Assuming each outer object has inner objects
-        const innerKeys = Object.keys(outerObject);
-    
-        $.each(innerKeys, function () {
-            const innerKey = this;
-            const innerValue = outerObject[innerKey];
-          $("#data_print").append(
-            `<tr>
-								<td>${this["outerKey"]}</td>
-								
-								<td>${this["innerValue"]}</td>
-														
-						</tr>`
-          );
-        });
-        console.log(newdata[this]);
-      });
-    })
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      const list = data.results;
+      list.map((item) => {
+        const id = item.id;
+        const adult = item.adult;
+        $("#data_print").append(
+          `<tr>
+            <td>${id}</td>
+            <td>${adult}</td>
+            </tr>`
+        );
+      })
+      })
 
     .catch((err) => console.error(err));
-}
+};
+
+
+
+
+
+
+
+
+
+
