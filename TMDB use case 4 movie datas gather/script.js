5bb563655cd6af068114caff0925f4f05ea10383
kaliyappan <141757796+Kaliyappan1@users.noreply.github.com>
2024-02-01 05:51:46 +0530

    Add files via upload

--- TMDB use case 4 movie datas gather/script.js
+++ TMDB use case 4 movie datas gather/script.js
@@ -1,78 +1,39 @@
-$(document).ready(function(){
-    
+// movie id
+fetch('https://api.themoviedb.org/3/movie/changes?page=1', {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MGE3ZDNmOGNiZmNiYTMwMjFjOGQwYTBkNTA1MDM2ZSIsInN1YiI6IjY1OWI1MTdiYTEwNzRiMDBlZWIwZmJhMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YLIYRQxwOX_1f3Pb4o332V5FQeZSMiae5EDo5Cuub_0'
+  }
+})
+  .then(movieId => movieId.json())
+  // .then(movieId => console.log(movieId))
+  .then(movieId => {
+    for (let i = 0; i < movieId.results.length; i++) {
+      let title= movieId.results[i].id;
+      // console.log(title);
 
-  // api key list
-  // let movie_list = 'https://api.themoviedb.org/3/movie/changes?page=1';
-  // let movie_details = 'https://api.themoviedb.org/3/movie/movie_id?language=en-US'
-  // let movie_genre = 'https://api.themoviedb.org/3/genre/movie/list?language=en' ;
-    
-  // api key loop
+      // movie details
 
-
-    // fetchMovieGenre();
-    // fetchMovieList();
-    // fetchMovieDetails();
-
-    // movie list api
-    const movieList = {
+      fetch("https://api.themoviedb.org/3/movie/"+title+"?language=en-US", {
         method: 'GET',
         headers: {
           accept: 'application/json',
           Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MGE3ZDNmOGNiZmNiYTMwMjFjOGQwYTBkNTA1MDM2ZSIsInN1YiI6IjY1OWI1MTdiYTEwNzRiMDBlZWIwZmJhMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YLIYRQxwOX_1f3Pb4o332V5FQeZSMiae5EDo5Cuub_0'
         }
-      };
-
-      
-      
-      fetch('https://api.themoviedb.org/3/movie/changes?page=1', movieList)
-        .then(response => response.json())
-        // .then(response => console.log(response))
-        .then(response => {
-        //  for (let i = 0; i < response.length+1; i++) {
-        //   const element = response[i];
-        //   console.log(element);
-          
-        //  }
-          $.each(response,function(item){
-            $.each(item,function (key,value){
-
-              console.log(key, value);
-            })
-          })
-            
+      })
+        .then(movieDetails => movieDetails.json())
+        // .then(movieDetails => console.log(movieDetails))
+        .then(movieDetails => {
+          for(i in movieDetails){
+            for(j in movieDetails[i].genres){
+              console.log(movieDetails.title);
+            }
+          }
           
         })
         .catch(err => console.error(err));
+    }
     
-        // movie details
-    
-        // const details = {
-        //     method: 'GET',
-        //     headers: {
-        //       accept: 'application/json',
-        //       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MGE3ZDNmOGNiZmNiYTMwMjFjOGQwYTBkNTA1MDM2ZSIsInN1YiI6IjY1OWI1MTdiYTEwNzRiMDBlZWIwZmJhMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YLIYRQxwOX_1f3Pb4o332V5FQeZSMiae5EDo5Cuub_0'
-        //     }
-        //   };
-          
-        //   fetch(movie_details, details)
-        //     .then(response => response.json())
-        //     .then(response => console.log(response))
-        //     .catch(err => console.error(err));
-
-
-    // movie genre api
-    // const movieGenre = {
-    //     method: 'GET',
-    //     headers: {
-    //       accept: 'application/json',
-    //       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MGE3ZDNmOGNiZmNiYTMwMjFjOGQwYTBkNTA1MDM2ZSIsInN1YiI6IjY1OWI1MTdiYTEwNzRiMDBlZWIwZmJhMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YLIYRQxwOX_1f3Pb4o332V5FQeZSMiae5EDo5Cuub_0'
-    //     }
-    //   };
-      
-    //   fetch(movie_genre, movieGenre)
-    //     .then(response => response.json())
-    //     .then(response => console.log(response))
-    //     .catch(err => console.error(err));
-    
-
-})
+  })
+  .catch(err => console.error(err));
